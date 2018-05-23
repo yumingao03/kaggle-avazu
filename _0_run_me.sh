19a839ecf7daab9c2f4c30492f1c30ef7e8b15f4
高玉敏 <gaoyumin@gaoyumindeMacBook-Pro.local>
2018-05-23 22:28:04 +0800

    add comment

--- _0_run_me.sh
+++ _0_run_me.sh
@@ -1,8 +1,11 @@
 #small test run using day 30 as validation
 python utils.py -set_params Y 1.0
+# 生成特征
 python _1_encode_cat_features.py
 python _2b_generate_dataset_for_vw_fm.py
 python _2c_generate_fm_features.py
+
+# 模型融合
 python _3a_rf.py
 python _3b_gbdt.py
 python _3c_vw.py -rseed 1
@@ -13,6 +16,7 @@ python _3d_fm.py -rseed 51
 python _3d_fm.py -rseed 52
 python _3d_fm.py -rseed 53
 python _3d_fm.py -rseed 54
+
 #should generate logloss ~= 0.3937
 python _4_post_processing.py
 
